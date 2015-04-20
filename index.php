0b6ed0d7780f3765e4a7c570141f46d9e1c74a60
MustafaSabur <musy.sabur@gmail.com>
2015-04-20 10:53:45 +0200

    start

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<title>Eenmaal Andermaal</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	
+  	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	
+</head>
+
+
+<body>
+
+	<header>
+		<div class="logo">LOGOBDkkkKKjjyyy</div>
+		<div class="login">Login</div>
+		<div class="login">LOGIN</div>
+	</header>
+
+	<nav>
+		Rubrieken
+	</nav>
+	<main>
+
+	</main>
+	<aside>
+		
+	</aside>
+
+	<footer>
+		
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+</body>
+</html>
