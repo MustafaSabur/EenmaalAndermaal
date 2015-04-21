7e736416ebf91d93faac8e394b57aa8135a82f0f
MustafaSabur <musy.sabur@gmail.com>
2015-04-21 09:34:08 +0200

    .

--- index.php
+++ index.php
@@ -4,33 +4,105 @@
 	<title>Eenmaal Andermaal</title>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
-  	
-  	<link rel="stylesheet" type="text/css" href="css/custom.css">
+ 	<link rel="stylesheet" type="text/css" href="css/custom.css">
 	
 </head>
 
 
 <body>
 
+<div class="container">
 	<header>
-		<div class="logo">LOGOBDkkkKKjjyyy</div>
+		<div class="logo"><img src="images/logo.png"></div>
+		<div class="keurmerk">Keurmerk</div>
 		<div class="login">Login</div>
-		<div class="login">LOGIN</div>
 	</header>
-
-	<nav>
-		Rubriekennenenenenenasdfasdfasdf
+</div>
+<div class="container">
+	<nav class="rubrieken">
+		<a href="#">All</a>
+		<a href="#">Cars</a>
+		<a href="#">Clothes</a>
+		<a href="#">Other</a>
 	</nav>
-	<main>
 
+	<main>
+		<h1>Last-Minutes</h1>
+		<div class="product-row">
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+		</div>
+
+		<h1>Popular</h1>
+		<div class="product-row">
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+		</div>
+
+		<h1>Recent Views</h1>
+		<div class="product-row">
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+			<div class="product">product</div>
+		</div>
 	</main>
+<</div>
 	<aside>
 		
 	</aside>
-
+<div class="container">
 	<footer>
-		
+		footerr
 	</footer>
+</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 </body>
 </html>
