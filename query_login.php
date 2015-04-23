a3b738fcf0e45423f3d868af02e7a065c6d38f10
svenvanwijnen <sven.van.wijnen1994@gmail.com>
2015-04-23 14:46:14 +0200

    inlogquery_aangepast

--- query_login.php
+++ query_login.php
@@ -2,36 +2,43 @@
 require 'connect.php';
 
 session_start();
-$required = array(
-    'gebruikersnaam',
-    'password'
-);
-
-$gebruikersnaam = $_POST['gebruikersnaam'];
-$password       = $_POST['password'];
-
 $required = array (
 	'gebruikersnaam',
 	'password'
 );
+
+$gebruikersnaam = $_POST['gebruikersnaam'];
+$password       = $_POST['password'];
 
 // Controleren of er verplichte velden leeggelaten zijn
 foreach ($required as $input)
 {
     if (empty($_POST[$input]))
     {
-		echo 'Er zijn een of meerdere verplichte velden leeggelaten. <br>';
+		echo 'De door u ingevulde combinatie komt niet voor in de database. Probeer het opnieuw.';
+		header("refresh:2;url=index.php");
         exit();
     }
+}
+
+// controleren of gebruikersnaam in gebruik is
+$sql = "SELECT GEBRUIKERSNAAM FROM GEBRUIKER WHERE GEBRUIKERSNAAM = '$gebruikersnaam'";
+$result = sqlsrv_query($conn, $sql, array(), array("Scrollable"=>"buffered"));
+$rowCount = sqlsrv_num_rows($result);
+
+if (empty($rowCount)) {
+	echo 'De door u ingevulde combinatie komt niet voor in de database. Probeer het opnieuw.';
+	header("refresh:2;url=index.php");
+	exit();
 }
 	
 	// SQL query
 	$tsql = "SELECT GEBRUIKERSNAAM, WACHTWOORD FROM GEBRUIKER WHERE GEBRUIKERSNAAM = '$gebruikersnaam'";
-	$result = sqlsrv_query($conn, $tsql, null);
+	$tresult = sqlsrv_query($conn, $tsql, null);
 	
 	// Controleren van gegevens dmv password_verify
 	// http://php.net/manual/en/function.password-verify.php
-	while($row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
+	while($row = sqlsrv_fetch_array( $tresult, SQLSRV_FETCH_ASSOC) ) {
 		if ($row['GEBRUIKERSNAAM'] == $gebruikersnaam && password_verify($password,$row['WACHTWOORD'])) {
 			echo 'U bent ingelogd!';
 			$_SESSION['loginnaam'] = $gebruikersnaam;
