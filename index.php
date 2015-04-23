03c53caec53ad5a46e3320c88afcda42e9f8dfab
MustafaSabur <musy.sabur@gmail.com>
2015-04-23 09:23:39 +0200

    bootstrap

--- index.php
+++ index.php
@@ -4,30 +4,32 @@
 	<title>Eenmaal Andermaal</title>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
- 	<link rel="stylesheet" type="text/css" href="css/custom.css">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+ 	<link rel="stylesheet" href="css/custom.css">
+
 	
 </head>
 
 
 <body>
 
-<div class="container">
+<div class="container-fluid">
 	<header>
 		<div class="logo"><img src="images/logo.png"></div>
 		<div class="keurmerk">Keurmerk</div>
 		<div class="login">Login</div>
 	</header>
 </div>
-<div class="container">
+<div class="container-fluid">
 	<main>
-		<nav class="rubrieken">
+		<nav class="col-md-2">
 			<a href="#">All</a>
 			<a href="#">Cars</a>
 			<a href="#">Clothes</a>
 			<a href="#">Other</a>
 		</nav>
 
-		<div class="content">
+		<div class="col-md-10">
 
 			<h1>Last-Minutes</h1>
 			<div class="product-row">
@@ -101,11 +103,12 @@
 	<aside>
 		
 	</aside>
-<div class="container">
+<div class="container-fluid">
 	<footer>
 		footerr
 	</footer>
 </div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </body>
 </html>
