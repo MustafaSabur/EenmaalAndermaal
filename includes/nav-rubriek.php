26e463a37548445db882a6290058e1e1f6ceda2f
MustafaSabur <musy.sabur@gmail.com>
2015-04-28 14:51:47 +0200

    .

---
+++ includes/nav-rubriek.php
@@ -0,0 +1,8 @@
+<nav class="col-xs-2 hidden-xs">
+	<h3>Rubrieken</h3>
+	<hr>
+	<a href="#">Alles</a>
+	<a href="#">Auto's</a>
+	<a href="#">Kleding</a>
+    <a href="#">Diversen</a>
+</nav>
--- index.php
+++ index.php
@@ -7,48 +7,15 @@
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
  	<link rel="stylesheet" href="css/custom.css">
-
-	
 </head>
 
 
 <body>
-
-	<?php require 'includes/header.php';?>
-<div class="container-fluid">
-	<div class="row row-nomargin zoekbalk">    
-        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-		    <div class="input-group">
-                <input type="hidden" name="search_param" value="all" id="search_param">         
-                <input type="text" class="form-control" name="x" placeholder="Zoek voorwerp...">
-                <div class="input-group-btn search-panel">
-                    <!-- <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    	<span id="search_concept">Rubriek</span> <span class="caret"></span>
-                    </button> -->
-                    <select name="Rubriek" class="btn"> <!-- class="dropdown-menu" role="menu" -->>
-                      <option>Alles</option>
-                      
-                    </select>
-                </div>
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search"></span></button>
-                </span>
-
-            </div>
-        </div>
-	</div>
-</div>
+<?php require 'includes/header.php';?>
+<?php require 'includes/zoekbalk.php';?>
 <div class="container-fluid">
 	<main class= "row row-nomargin">
-		<nav class="col-xs-2 hidden-xs">
-			<h3>Rubrieken</h3>
-			<hr>
-			<a href="#">Alles</a>
-			<a href="#">Auto's</a>
-			<a href="#">Kleding</a>
-            <a href="#">Diversen</a>
-		</nav>
-
+		<?php require 'includes/nav-rubriek.php';?>
 		<div class="content col-xs-12 col-sm-10">
 
 			<h1>Last-Minutes</h1>
