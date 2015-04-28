5088930ec4a14c0b73396d26b367504b20c4b6cf
bountielol <hackyhacker@Live.nl>
2015-04-28 15:00:41 +0200

    tom

---
+++ productoverzicht.php
@@ -0,0 +1,104 @@
+<?php
+require 'includes/connect.php';
+require 'includes/header.php';
+?>		
+		<!doctype html>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/custom.css">
+		<link rel="stylesheet" href="css/productoverzicht.css">
+
+		<div class="container-fluid">
+		<div class="col-xs-6">
+		
+				<div id="page">
+		
+			<div id="productenzoeken">
+			
+				<input type="submit" value="Zoek product">
+				<input type="text" name="fname">
+				
+			</div>
+			
+		</div>
+		
+		<div id="productenoverzicht">
+		
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product1-01.jpg" alt="Muis">
+				<h5>Beschrijving:</h5>
+				<h4>€ 25.25</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product2-01.jpg" alt="Speaker">
+				<h5>Beschrijving:</h5>
+				<h4>€ 19.99</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product3-01.jpg" alt="Fiets">
+				<h5>Beschrijving:</h5>
+				<h4>€ 19.99</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product4-01.jpg" alt="Macbook">
+				<h5>Beschrijving:</h5>
+				<h4>€ 19.99</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product5-01.jpg" alt="Romy">
+				<h5>Beschrijving:</h5>
+				<h4>€ 19.99</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product6-01.jpg" alt="Test">
+				<h5>Beschrijving:</h5>
+				<h4>€ 19.99</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product7-01.jpg" alt="Test">
+				<h5>Beschrijving:</h5>
+				<h4>€ 19.99</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product8-01.jpg" alt="Test">
+				<h5>Beschrijving:</h5>
+				<h4>€ 19.99</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product9-01.jpg" alt="Test">
+				<h5>Beschrijving:</h5>
+				<h4>€ 19.99</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+			<section class= "overzichtitem">
+				<img src= "images/voorwerpen/product10-01.jpg" alt="Test">
+				<h5>Beschrijving:</h5>
+				<h4>€ 19.99</h4>
+				<input type="submit" value="Bied mee">
+			</section>
+			
+		
+		</div>
+
+<?php
+require 'includes/footer.php'
+?>
