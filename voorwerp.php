7aa5e772c8f8b920f54dba495fa1d506d0419b59
MustafaSabur <musy.sabur@gmail.com>
2015-04-29 13:07:46 +0200

    .

--- voorwerp.php
+++ voorwerp.php
@@ -7,19 +7,27 @@
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
  	<link rel="stylesheet" href="css/custom.css">
+ 	<link rel="stylesheet" href="css/voorwerp.css">
 </head>
 <body>
 	<?php require 'includes/header.php';?>
 	<?php require 'includes/zoekbalk.php';?>
-	<div class="container-fruid">
+	<div class="container-fluid">
 		<div class="row row-nomargin">
 			<?php require 'includes/nav-rubriek.php';?>
 			<div class="content col-sm-10">
 				<h1>Muis</h1>
-			
-				
-				<img src="images/voorwerpen/product1-01.jpg" alt="foto muis">
-				
+				<div class="row">
+					<div class="col-xs-6">
+						<img src="images/voorwerpen/product1-01.jpg" alt="foto muis">
+					</div>
+					<div class="col-xs-6 test">
+						<div class="timer center-block">
+						time
+							
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
