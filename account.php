e5e41da6845e89175f2bc67db8867c2cdf19ae66
svenvanwijnen <sven.van.wijnen1994@gmail.com>
2015-04-30 14:30:48 +0200

    account errorpage

--- account.php
+++ account.php
@@ -25,48 +25,53 @@ echo '
 <table class="table table-hover table-responsive">';
 
 if ($conn) {
-	$session = $_SESSION['loginnaam'];
-	$info = array (
-	'GEBRUIKERSNAAM',
-	'VOORNAAM',
-	'ACHTERNAAM',
-	'ADRESREGEL1',
-	'ADRESREGEL2',
-	'POSTCODE',
-	'LAND',
-	'MAILBOX',
-	'IS_VERKOPER'
-	);
+	if (!isset($_SESSION['loginnaam'])) {
+		echo '<h3><small>U bent niet ingelogd. Log in om uw accountgegevens te bekijken.</h3></small></table>';
+	}
 	
-	$display = array (
-	'Gebruikersnaam',
-	'Voornaam',
-	'Achternaam',
-	'Adres 1',
-	'Adres 2',
-	'Postcode',
-	'Land',
-	'E-mail',
-	'Ik ben verkoper'
-	);
-	
-	$sql = "SELECT GEBRUIKERSNAAM, VOORNAAM, ACHTERNAAM, ADRESREGEL1, ADRESREGEL2, POSTCODE, LAND, MAILBOX, IS_VERKOPER from GEBRUIKER where GEBRUIKERSNAAM = '$session'";
-	$result = sqlsrv_query($conn, $sql, null);
+	else {
+		$info = array (
+		'GEBRUIKERSNAAM',
+		'VOORNAAM',
+		'ACHTERNAAM',
+		'ADRESREGEL1',
+		'ADRESREGEL2',
+		'POSTCODE',
+		'LAND',
+		'MAILBOX',
+		'IS_VERKOPER'
+		);
 		
-		if( (sqlsrv_errors()) != null) {
-			echo '<h1><small>Er is iets foutgegaan aan onze kant. Probeer het later opnieuw.</small></h1>';
+		$display = array (
+		'Gebruikersnaam',
+		'Voornaam',
+		'Achternaam',
+		'Adres 1',
+		'Adres 2',
+		'Postcode',
+		'Land',
+		'E-mail',
+		'Ik ben verkoper'
+		);
+		
+		$session = $_SESSION['loginnaam'];
+		$sql = "SELECT GEBRUIKERSNAAM, VOORNAAM, ACHTERNAAM, ADRESREGEL1, ADRESREGEL2, POSTCODE, LAND, MAILBOX, IS_VERKOPER from GEBRUIKER where GEBRUIKERSNAAM = '$session'";
+		$result = sqlsrv_query($conn, $sql, null);
+			
+			if( (sqlsrv_errors()) != null) {
+				echo '<h1><small>Er is iets foutgegaan aan onze kant. Probeer het later opnieuw.</small></h1>';
+			}
+		
+		while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+			for ($i = 0; $i < 9; $i++) {
+					echo '<tr><td>'.$display[$i].':</td> <td>'.$row[$info[$i]].'</td></tr>';
+			}
 		}
-	
-	while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-		for ($i = 0; $i < 9; $i++) {
-				echo '<tr><td>'.$display[$i].':</td> <td>'.$row[$info[$i]].'</td></tr>';
-		}
+		echo '</table>';
+		echo '<button type="button" class="btn btn-primary">Wijzigen</button>';
 	}
 }
 ?>
-
-</table>
-<button type="button" class="btn btn-primary">Wijzigen</button>
 </div>
 </div>
 </div>
