ae0cc79d4fce22be4a01ed6cfe916bd5b38928ba
bountielol <hackyhacker@Live.nl>
2015-04-30 14:14:31 +0200

    werkt niet

--- productoverzicht.php
+++ productoverzicht.php
@@ -15,93 +15,116 @@
 <?php require 'includes/header.php';?>
 <?php require 'includes/zoekbalk.php';?>
 <div class="container-fluid">
-	<main class= "row row-nomargin">
+	<main class= "row">
 	<?php require 'includes/nav-rubriek.php';?>
-	<div id="productenoverzicht">
-		
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product1-01.jpg" alt="Muis">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				</a>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+	<div class="content col-xs-10">
+		<div class="row">
+			<section class="col-xs-6 productBox">
+				<div class="row">
+					<div class="col-xs-4 box-img">
+						<img class="plaatje" src="images/voorwerpen/product1-01.jpg" alt="Muis">
+					</div>
+					<div class="col-xs-8">
+					<h3>1234567890qwertyuiopasd</h3>
+					</div>
+					<p>Beschrijving: </p>  
+					<p>Een computer muis</p>
+					<p>Prijs: €20</p>
+					<input type="submit"  value="Bied mee">
+				</div>
 			</section>
 			
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product2-01.jpg" alt="Speaker">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+			<section class= "col-xs-6">
+				<div class="productBox">
+					<img class= "plaatje" src= "images/voorwerpen/product2-01.jpg" alt="Speaker">
+					<h4>Beschrijving: </h4>  
+					<h5>Een computer muis</h5>
+					<h3>Prijs: €20</h3>
+					<input type="submit" value="Bied mee">
+				</div>
 			</section>
 			
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product3-01.jpg" alt="Fiets">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+			<section class= "col-xs-6">
+				<div class="productBox">
+					<img class= "plaatje" src= "images/voorwerpen/product3-01.jpg" alt="Fiets">
+					<h4>Beschrijving: </h4>  
+					<h5>Een computer muis</h5>
+					<h3>Prijs: €20</h3>
+					<input type="submit" value="Bied mee">
+				</div>
 			</section>
 			
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product4-01.jpg" alt="Macbook">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+			<section class= "col-xs-6">
+				<div class="productBox">
+					<img class= "plaatje" src= "images/voorwerpen/product4-01.jpg" alt="Macbook">
+					<h4>Beschrijving: </h4>  
+					<h5>Een computer muis</h5>
+					<h3>Prijs: €20</h3>
+					<input type="submit" value="Bied mee">
+				</div>
 			</section>
 			
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product5-01.jpg" alt="Romy">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+			<section class= "col-xs-6">
+				<div class="productBox">
+					<img class= "plaatje" src= "images/voorwerpen/product5-01.jpg" alt="Romy">
+					<h4>Beschrijving: </h4>  
+					<h5>Een computer muis</h5>
+					<h3>Prijs: €20</h3>
+					<input type="submit" value="Bied mee">
+				</div>
 			</section>
 			
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product6-01.jpg" alt="Test">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+			<section class= "col-xs-6">
+				<div class="productBox">
+					<img class= "plaatje" src= "images/voorwerpen/product6-01.jpg" alt="Test">
+					<h4>Beschrijving: </h4>  
+					<h5>Een computer muis</h5>
+					<h3>Prijs: €20</h3>
+					<input type="submit" value="Bied mee">
+				</div>
 			</section>
 			
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product7-01.jpg" alt="Test">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+			<section class= "col-xs-6">
+				<div class="productBox">
+					<img class= "plaatje" src= "images/voorwerpen/product7-01.jpg" alt="Test">
+					<h4>Beschrijving: </h4>  
+					<h5>Een computer muis</h5>
+					<h3>Prijs: €20</h3>
+					<input type="submit" value="Bied mee">
+				</div>
 			</section>
 			
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product8-01.jpg" alt="Test">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+			<section class= "col-xs-6">
+				<div class="productBox">
+					<img class= "plaatje" src= "images/voorwerpen/product8-01.jpg" alt="Test">
+					<h4>Beschrijving: </h4>  
+					<h5>Een computer muis</h5>
+					<h3>Prijs: €20</h3>
+					<input type="submit" value="Bied mee">
+				</div>
 			</section>
 			
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product9-01.jpg" alt="Test">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+			<section class= "col-xs-6 productBox">
+				<div class="productBox">
+					<img class= "plaatje" src= "images/voorwerpen/product9-01.jpg" alt="Test">
+					<h4>Beschrijving: </h4>  
+					<h5>Een computer muis</h5>
+					<h3>Prijs: €20</h3>
+					<input type="submit" value="Bied mee">
+				</div>
 			</section>
 			
-			<section class= "overzichtitem">
-				<img src= "images/voorwerpen/product10-01.jpg" alt="Test">
-				<h4>Beschrijving: </h4>  
-				<h5>Een computer muis</h5>
-				<h3>Prijs: €20</h3>
-				<input type="submit" value="Bied mee">
+			<section class= "col-xs-6 productBox">
+				<div class="productBox">
+					<img class= "plaatje" src= "images/voorwerpen/product10-01.jpg" alt="Test">
+					<h4>Beschrijving: </h4>  
+					<h5>Een computer muis</h5>
+					<h3>Prijs: €20</h3>
+					<input type="submit" value="Bied mee">
+				</div>
 			</section>
-			
-		</div>
-		
+		</div>	
+	</div>	
 	</main>
 </div>
 <?php include 'includes/footer.php';?>
