3b9675e04ec2eb0c4d7b59f5091259100b49e04c
MustafaSabur <musy.sabur@gmail.com>
2015-05-12 11:04:50 +0200

    .

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
 <body>
 
-	<?php require 'includes/header.php';?>
+<?php require 'includes/header.php';?>
 <div class="container-fluid">
 	<div class="row zoekbalk">    
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -22,7 +22,7 @@
                     <!-- <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
                     	<span id="search_concept">Rubriek</span> <span class="caret"></span>
                     </button> -->
-                    <select name="Rubriek" class="btn"> <!-- class="dropdown-menu" role="menu" -->>
+                    <select name="Rubriek" class="btn"> <!-- class="dropdown-menu" role="menu" -->
                       <option>Alles</option>
                       
                     </select>
@@ -45,11 +45,13 @@
 				<div class="product-row" id="l-minute">
 
 					<a href="voorwerp.php" class="product">
+
 						<div class="product-img ">
 							<img src="images/voorwerpen/product1-01.jpg" alt="iphone">
 						</div>
 						<h5>USB Muis</h5>
 						<h4>Hoogste bod: € 180,00</h4>
+						<p class="time" id="time"></p>
 					</a>
 					<a href="voorwerp.php" class="product">
 						<div class="product-img ">
@@ -57,6 +59,7 @@
 						</div>
 						<h5>USB Muis</h5>
 						<h4>Hoogste bod: € 180,00</h4>
+						<p class="time" id="time2"></p>
 					</a>
 					<div class="product">product</div>
 					<div class="product">product</div>
@@ -83,7 +86,7 @@
 							</div>
 							<h5>USB Muis</h5>
 							<h4>Hoogste bod: € 180,00</h4>
-							<p id="time">f</p>
+							
 					</a>
 					<div class="product">product</div>
 					<div class="product">product</div>
