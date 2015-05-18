be454c52845b2c5d4ab7d5fd35c1bdc5357eb3e2
MustafaSabur <musy.sabur@gmail.com>
2015-05-18 23:54:30 +0200

    .

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,8 @@
 <div class="container-fluid">
 	<header class="row" id="header">
-		<div class="logo col-xs-12 col-sm-6" id="logo"><a href="index.php"><img src="images/logo.png" alt="logo"></a></div>
-		<div class="keurmerk col-md-2 hidden-sm hidden-xs right" id="keurmerk"><img src="images/keurmerk.png" alt="keurmerk"></div>
+		<div class="logo col-xs-12 col-sm-6 col-sm-push-0" id="logo"><a href="index.php"><img src="images/logo.png" alt="logo"></a></div>
+		<div class="header-text col-md-1 hidden-sm hidden-xs" id="keurmerk">Meer dan<h4>123000+</h4>artikelen</div>
+		<div class="keurmerk col-md-1 hidden-sm hidden-xs" id="keurmerk"><img src="images/keurmerk.png" alt="keurmerk"></div>
 		<div class="login col-xs-12 col-sm-6 col-md-4" id="login"><?php require 'login.php';?></div>
 	</header>
 </div>
