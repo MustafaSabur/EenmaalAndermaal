886ce34db5ae9aef96f0a7b096e91cbd6f6c6a8d
MustafaSabur <musy.sabur@gmail.com>
2015-05-18 21:08:18 +0200

    .

--- includes/login.php
+++ includes/login.php
@@ -4,20 +4,21 @@ session_start();
 if (!isset($_SESSION['loginnaam'])) {
 ?>
         <form class="form-horizontal" method="post" action="query_login.php">
-	        <div class="form-group col-xs-5 col-sm-12" id="name">	
+	        <div class="form-group col-xs-5 col-sm-9" id="name">	
 				<input type="text" class="form-control" name="gebruikersnaam" placeholder="Gebruikersnaam">
 			</div>
-			<div class="form-group col-xs-5 col-sm-12" id="password">
-				<input type="password" class="form-control" name="password" placeholder="Wachtwoord">
-			</div>
-			<div class="form-group col-xs-2 col-sm-4 col-sm-push-8" id="login-submit">
-				<button type="submit" class="btn btn-success" name="sumbit">Log In</button>
-			</div>
-			<div class="form-group login-tekst col-xs-12 col-sm-8 col-sm-pull-4" id="login-links">
+			<div class="form-group login-tekst col-xs-12 col-sm-3" id="login-links">
 				
 				<a href="vergeten.php" class="login-tekst">Vergeten?</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 				<a href="register.php" class="login-tekst">Registreren</a>
 			</div>
+			<div class="form-group col-xs-5 col-sm-9" id="password">
+				<input type="password" class="form-control" name="password" placeholder="Wachtwoord">
+			</div>
+			<div class="form-group col-xs-2 col-sm-3" id="login-submit">
+				<button type="submit" class="btn btn-success" name="sumbit">Log In</button>
+			</div>
+			
 			
         </form>
 <?php    
@@ -29,6 +30,6 @@ else {
 		<a href="toevoegen-artikel.php">Veiling toevoegen</a><br>
 		<a href="account.php">Mijn account</a><br>
 		<a href="logout.php">Log uit</a>';
-}
+	}
 ?>
 </div>
