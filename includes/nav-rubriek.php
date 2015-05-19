34f2a4ca3debdc0bb66e196bf7dfec5771348159
VolkyYuksel <v.yuksel@student.han.nl>
2015-05-19 15:07:41 +0200

    opmaak

--- includes/nav-rubriek.php
+++ includes/nav-rubriek.php
@@ -1,8 +1,21 @@
 <nav id="nav">
 	<h3>Rubrieken</h3>
 	<hr>
-	<a href="#">Alles</a>
-	<a href="#">Auto's</a>
-	<a href="rubriek.php">Computers</a>
-    <a href="#">Diversen</a>
+	<a href="#">Antiek en Kunst</a>
+	<a href="#">Audio, Tv en Foto</a>
+    <a href="#">Auto's</a>
+    <a href="#">Auto Diversen</a>
+    <a href="#">Boeken</a>
+    <a href="#">Cd's en Dvd's</a>
+    <a href="rubriek.php">Computers en Software</a>
+    <a href="#">Fietsen en Brommers</a>
+    <a href="#">Huis en Inrichting</a>
+    <a href="#">Kleding</a>
+    <a href="#">Sieraden en Tassen</a>
+    <a href="#">Spelcomputers en Games</a>
+    <a href="#">Sport en Fitness</a>
+    <a href="#">Vakantie</a>
+    <a href="#">Verzamelen</a>
+    <a href="#">Watersport en Boten</a>
+    <a href="#">Zakelijke goederen</a>
 </nav>
