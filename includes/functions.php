8b8b4ef53cadcfb183c6efec4035db2ba64e5dd4
svenvanwijnen <sven.van.wijnen1994@gmail.com>
2015-05-28 13:00:49 +0200

    sdadasas

--- includes/functions.php
+++ includes/functions.php
@@ -338,6 +338,15 @@ function getZoekSuggesties($zoekterm, $inRubriek){
     }
 }
 
+function genRandomString($length = 15) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
 
 
