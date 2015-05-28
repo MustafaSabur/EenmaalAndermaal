155df588995a752be2a3ef2f32cbe66becafe15a
unknown <tom>
2015-05-28 11:41:57 +0200

    asdfjkl

---
+++ connect.php
@@ -0,0 +1,5 @@
+<?php
+	$serverName = "(local)\sqlexpress";
+	$connectionInfo = array("Database"=>"EenmaalAnderMaal", "UID"=>"sa", "PWD"=>"P@ssw0rd");
+	$conn = sqlsrv_connect( $serverName, $connectionInfo);
+?>
