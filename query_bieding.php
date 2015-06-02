c8c2bd617c75bcd79389ea6df80f7d7d99dfa3fb
svenvanwijnen <sven.van.wijnen1994@gmail.com>
2015-06-02 14:50:25 +0200

    bieden

--- query_bieding.php
+++ query_bieding.php
@@ -1,20 +1,61 @@
  <?php
-    $session = $_SESSION['loginnaam'];
-    if(!isset($session))
-    {
-        echo 'U moet eerst inloggen voordat u kan bieden.';
-    }
-    else
-    {
-    $bedrag = $_POST['InputBedrag'];
-    $gebruiker = $session;
-    $voorwerp = $_POST['voorwerpID'];
-    $rubriek = $_POST['rubriekID'];
-     $sql = "insert into bod(voorwerp, bodbedrag, gebruiker)
-     values($voorwerp, $bedrag, $gebruiker)";
-    $conn = dbConnected();
+session_start();
+$session = $_SESSION['loginnaam'];
+$input_check = false;
+
+require 'includes/connect.php';	
+require 'includes/functions.php';
+
+if(!isset($session))
+{
+	echo 'U moet eerst inloggen voordat u kan bieden.';
+}
+else
+{
+	$bedrag = $_POST['InputBedrag'];
+	$gebruiker = $session;
+	$voorwerp = $_POST['voorwerpID'];
+	$rubriek = $_POST['rubriekID'];
+}
+
+$sql = "SELECT verkoper FROM voorwerp WHERE voorwerpnummer = '$voorwerp'";
+$result = sqlsrv_query($conn, $sql, null);
+
+while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+	if ($row['verkoper'] == $gebruiker) {
+		$input_check = false;
+		echo 'U mag niet op uw eigen voorwerpen bieden.';
+		header('refresh:2; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+	}
+}
+
+if (!is_numeric($bedrag)) {
+	echo 'Het door u opgegeven bedrag is niet geldig.';
+	$input_check = false;
+	header('refresh:2; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+	exit();
+}
+
+// var_dump($voorwerp);
+// var_dump($bedrag);
+// var_dump($gebruiker);
+
+if ($input_check == true) {
+    $sql = "INSERT INTO [dbo].[Bod] 
+			([voorwerp],
+			[bodbedrag],
+			[gebruiker]
+			)
+			values
+			($voorwerp,
+			$bedrag,
+			'$gebruiker')";
+	
     $result = sqlsrv_query($conn, $sql, null);
-    dbClose($conn);
-    header('refresh:5; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
-    }
+	
+	if( ($errors = sqlsrv_errors() ) != null) {
+		echo '<h3>Er is iets foutgegaan aan onze kant. Probeer het later opnieuw.</h3>';
+	}
+    header('refresh:3; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+}
 ?>
