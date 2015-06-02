f576a543b559006d58035b911f3494021ccd9677
unknown <tom>
2015-06-02 14:20:07 +0200

    blabla

--- query_bieding.php
+++ query_bieding.php
@@ -1,6 +1,5 @@
  <?php
-
-    echo $_SESSION['loginnaam'];
+    $session = $_SESSION['loginnaam'];
     if(!isset($session))
     {
         echo 'U moet eerst inloggen voordat u kan bieden.';
@@ -12,13 +11,10 @@
     $voorwerp = $_POST['voorwerpID'];
     $rubriek = $_POST['rubriekID'];
      $sql = "insert into bod(voorwerp, bodbedrag, gebruiker)
-     values($bedrag, $bedrag, $gebruiker)";
-     var_dump($voorwerp);
-     var_dump($rubriek);
+     values($voorwerp, $bedrag, $gebruiker)";
     $conn = dbConnected();
     $result = sqlsrv_query($conn, $sql, null);
     dbClose($conn);
     header('refresh:5; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
     }
-    
 ?>
