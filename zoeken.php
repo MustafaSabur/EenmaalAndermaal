5bcbb6131272b44330e0e7810e61e4bb83621127
MustafaSabur <musy.sabur@gmail.com>
2015-06-02 11:53:01 +0200

    .

---
+++ zoeken.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<title>Eenmaal Andermaal</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+ 	<link rel="stylesheet" href="css/custom.css">
+	<link rel="stylesheet" href="css/rubriek.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
+	<?php require 'includes/functions.php'; ?>
+</head>
+
+
+<body>
+<?php require 'includes/header.php';?>
+<?php require 'includes/zoekbalk.php';?>
+<div class="container-fluid">
+	<main class= "row">
+		
+		<div class="content">
+			<div class=" stroke-view">
+			<?php 
+				if (isset($_GET['page'])) {
+					getRubriekArtikelen($_GET['rub_nr'], $_GET['page']);
+				}
+				else getRubriekArtikelen($_GET['rub_nr']); 
+			?>
+			</div>
+			<div>
+			<?php 
+			  	if (isset($_GET['page'])) {
+					getPager($_GET['rub_nr'], $_GET['page'], $_GET['term']);
+				}else getPager($_GET['rub_nr']); 
+
+			?>
+			</div>
+
+		</div>	
+	</main>
+</div>
+<?php include 'includes/footer.php';?>
+<?php require 'includes/nav-rubriek.php';?>
+
+</body>
+</html>
