247bbc2d1d6abe82619cd59eab9e4c66ed3987c9
unknown <tom>
2015-06-04 14:35:45 +0200

    blab

--- query_bieding.php
+++ query_bieding.php
@@ -1,10 +1,25 @@
- <?php
-session_start();
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+	<title>query_bieding</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+ 	<link rel="stylesheet" href="css/custom.css">
+ 	<link rel="stylesheet" href="css/product-box.css">
+ 	<link rel="stylesheet" href="css/artikel.css">
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
+ 	<?php 	require 'includes/connect.php';
+ 			require 'includes/functions.php';	 
+ 			require 'includes/header.php';
+
+	?>
+</head>
+<?php
 $session = $_SESSION['loginnaam'];
 $input_check = true;
-
-require 'includes/connect.php';	
-require 'includes/functions.php';
 
 if(!isset($session))
 {
@@ -16,6 +31,7 @@ else
 	$gebruiker = $session;
 	$voorwerp = $_POST['voorwerpID'];
 	$rubriek = $_POST['rubriekID'];
+	$huidigBod = $_POST['hoogsteBod'];
 }
 
 $sql = "SELECT verkoper FROM voorwerp WHERE voorwerpnummer = '$voorwerp'";
@@ -23,22 +39,28 @@ $result = sqlsrv_query($conn, $sql, null);
 
 while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
 	if ($row['verkoper'] == $gebruiker) {
-		$input_check = false;
 		echo 'U mag niet op uw eigen voorwerpen bieden.';
 		header('refresh:3; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+		$input_check = false;
 	}
 }
 
 if (!is_numeric($bedrag)) {
 	echo 'Het door u opgegeven bedrag is niet geldig.';
-	$input_check = false;
 	header('refresh:3; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
-	exit();
+	$input_check = false;
 }
 
-// var_dump($voorwerp);
-// var_dump($bedrag);
-// var_dump($gebruiker);
+if($bedrag <= $huidigBod)
+{
+	echo 'Het door u ingegeven bedrag is kleiner/gelijk aan het huidige bodbedrag. <br>
+			Biedingen moeten hoger zijn dan het huidige bodbedrag.
+			';
+	header('refresh:3; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+	$input_check = false; 
+}
+
+checkVerhoging($huidigBod, $bedrag);
 
 if ($input_check == true) {
     $sql = "INSERT INTO [dbo].[Bod] 
@@ -62,5 +84,58 @@ if ($input_check == true) {
         }
 	}
     header('refresh:0; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+}
+
+
+
+
+function checkVerhoging ($huidigBod, $bedrag)
+{
+	if ($huidigBod < 50.00 && $huidigBod > 0.00) 
+	{
+		if ($bedrag + 0.50 < $huidigBod) 
+		{
+			$input_check = false;
+			echo 'bedrag is te laag 0-50';
+			//header('refresh:3; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+		}
+	}
+	if ($huidigBod < 500.00 && $huidigBod > 50.00) 
+	{
+		if ($bedrag + 1.00 < $huidigBod) 
+		{
+			$input_check = false;
+			echo 'bedrag is te laag 50-500';
+			//header('refresh:3; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+		}
+	}
+	if ($huidigBod < 1000.00 && $huidigBod > 500.00) 
+	{
+		if ($bedrag + 5.00 < $huidigBod) 
+		{
+			$input_check = false;
+			echo 'bedrag is te laag 500-1000';
+			//header('refresh:3; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+		}
+	}
+	if ($huidigBod < 5000.00 && $huidigBod > 1000.00) 
+	{
+		if ($bedrag + 10.00 < $huidigBod) 
+		{
+			$input_check = false;
+			echo 'bedrag is te laag 1000-5000';
+			//header('refresh:3; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+		}
+	}
+	if ($huidigBod > 5000.00) 
+	{
+		if ($bedrag + 50.00 < $huidigBod) 
+		{
+			$input_check = false;
+			echo 'bedrag is te laag 5000+';
+			//header('refresh:3; url= artikel.php?id='.$voorwerp.'&rub_nr='.$rubriek);
+		}
+	}
+
 }
 ?>
